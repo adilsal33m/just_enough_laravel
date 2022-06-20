b00b486c3c06f61c4b23990c2e622893d159aea2
adilsaleem92 <asaleem@fortewiz.com>
2022-06-20 06:40:03 +0500

    Show view handled through Resource Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\NotesController;
+use App\Models\Note;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('notes',NotesController::class);
+
+
+
+
+
+
+
+
+
+
+
+
+
